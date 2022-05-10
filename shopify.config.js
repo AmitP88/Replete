555f9a16ce5d7c1bfceb0e568ed10282780cf3bb
Amit Patel <amitpatel@Amits-MacBook-Pro.local>
2022-05-10 14:24:10 -0700

    added Storefront API password to config

--- shopify.config.js
+++ shopify.config.js
@@ -1,5 +1,5 @@
 export default {
   storeDomain: 'hydrogen-preview.myshopify.com',
-  storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+  storefrontToken: 'b6f98552aba607573a1d3eb7853de390',
   storefrontApiVersion: '2022-04',
 };
