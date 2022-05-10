049c8d0ee3e95af037f68188b12dfdd2e4b3dd42
Amit Patel <amitpatel@Amits-MacBook-Pro.local>
2022-05-10 14:26:31 -0700

    added store domain to config

--- shopify.config.js
+++ shopify.config.js
@@ -1,5 +1,5 @@
 export default {
-  storeDomain: 'hydrogen-preview.myshopify.com',
+  storeDomain: 'replete-hydrogen.myshopify.com',
   storefrontToken: 'b6f98552aba607573a1d3eb7853de390',
   storefrontApiVersion: '2022-04',
 };
