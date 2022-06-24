4e37ab12ba18885f8d932177d9f9ff4b3ef6690a
Amit Patel <amitpatel@Amits-MacBook-Pro.local>
2022-06-24 11:01:17 -0700

    resolved spacing error

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,7 +12,9 @@ export default function Banner() {
       style={banner_bg_properties}
     >
       <div className="banner__content w-fit m-auto text-center">
-        <h1 className="text-white text-3xl sm:text-5xl w-10/12 sm:w-full font-bold m-auto mb-10">Inspiring products for your life</h1>
+        <h1 className="text-white text-3xl sm:text-5xl w-10/12 sm:w-full font-bold m-auto mb-10">
+          Inspiring products for your life
+        </h1>
         <p className="m-auto w-9/12 md:w-[490px] text-white text-left mb-10">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
