3768950139d3337dfeaaf4f0eaff06d0508b637b
Amit Patel <amitpatel@Amits-MacBook-Pro.local>
2022-06-29 17:19:43 -0700

    made Subscribe component responsive

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,8 +1,8 @@
 export default function Subscribe() {
   return (
     <div className="Subscribe bg-[#f81a1a]">
-      <div className="lg:w-full lg:max-w-6xl lg:flex m-auto justify-center py-12">
-        <h1 className="text-white text-left sm:text-4xl lg:text-3xl w-8 sm:w-full font-bold m-auto">
+      <div className="w-full lg:max-w-6xl lg:flex m-auto justify-center text-center lg:text-base py-12">
+        <h1 className="text-white text-center lg:text-left sm:text-4xl lg:text-3xl sm:w-full font-bold m-auto pb-3 lg:pb-0">
           Subscribe to our weekly newsletter
         </h1>
         <button className="text-white text-xs lg:text-base font-bold bg-[#f81a1a] h-auto w-fit px-12 py-2">
